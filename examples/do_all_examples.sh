4468013a5fb72f202bbab8fd804afd0dbd47d502
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2026-04-15 11:16:09 +0300

    Running with pure dftsolve commands

--- examples/do_all_examples.sh
+++ examples/do_all_examples.sh
@@ -8,56 +8,56 @@ SCRIPTPATH=`dirname $SCRIPT`
 # Bulk-Al-noCIF -------------------
 echo "Adding: Bulk-GaAs-noCIF."
 cd ./Bulk-GaAs-noCIF
-tsp mpirun -np $CORENUMBER dftsolve.py -i bulk_gaas.py
+tsp dftsolve -p $CORENUMBER -i bulk_gaas.py
 
 # Cr2O-spin -------------------
 echo "Adding: Cr2O-spin"
 cd ../Cr2O-spin
-tsp mpirun -np $CORENUMBER dftsolve.py -i Cr2O.py -g Cr2O_mp-1206821_primitive.cif
+tsp dftsolve -p $CORENUMBER -i Cr2O.py -g Cr2O_mp-1206821_primitive.cif
 
 # Graphene-LCAO -------------
 echo "Adding: Graphene-LCAO."
 cd ../Graphene-LCAO
 echo "Step 1: Pristine graphene."
-tsp mpirun -np $CORENUMBER dftsolve.py -i graphene.py -g graphene4x4.cif
+tsp dftsolve -p $CORENUMBER -i graphene.py -g graphene4x4.cif
 echo "Step 2: Graphene with defect."
-tsp mpirun -np $CORENUMBER dftsolve.py -i graphene.py -g graphene4x4withdefect.cif
+tsp dftsolve -p $CORENUMBER -i graphene.py -g graphene4x4withdefect.cif
 
 # Graphene-charged -------------
 echo "Adding: Graphene-charged."
 cd ../Graphene-charged
 echo "Step 1: Neutral defective graphene."
-tsp mpirun -np $CORENUMBER dftsolve.py -i graphene-neutral.py -g graphene4x4withdefect.cif
+tsp dftsolve -p $CORENUMBER -i graphene-neutral.py -g graphene4x4withdefect.cif
 echo "Step 2: Charged defective Graphene."
-tsp mpirun -np $CORENUMBER dftsolve.py -i graphene-charged.py -g graphene4x4withdefect.cif
+tsp dftsolve -p $CORENUMBER -i graphene-charged.py -g graphene4x4withdefect.cif
 
 # Si-2atoms-optical ----------------
 echo "adding: Si-2atoms-optical"
 cd ../Si-2atoms-optical
 echo "Step 1: Ground, DOS, and Band."
-tsp mpirun -np $CORENUMBER dftsolve.py -i Si-Step1-ground_dos_band.py -g Si_mp-149_primitive_Example.cif
+tsp dftsolve -p $CORENUMBER -i Si-Step1-ground_dos_band.py -g Si_mp-149_primitive_Example.cif
 echo "Step 2: Optical - RPA."
-tsp dftsolve.py -i Si-Step2-optical-RPA.py -g Si_mp-149_primitive_Example.cif
+tsp dftsolve -i Si-Step2-optical-RPA.py -g Si_mp-149_primitive_Example.cif
 echo "Step 3: Optical - BSE."
-tsp dftsolve.py -i Si-Step3-optical-BSE.py -g Si_mp-149_primitive_Example.cif
+tsp dftsolve -i Si-Step3-optical-BSE.py -g Si_mp-149_primitive_Example.cif
 
 # Wurtzite ZnO with DFT+U
 echo "Adding: ZnO with DFT+U."
 cd ../ZnO-with-Hubbard
 echo "Step 1: Ground, DOS, and Band with DFT+U."
-tsp mpirun -np $CORENUMBER dftsolve.py -i ZnO_withHubbard.py
+tsp dftsolve -p $CORENUMBER -i ZnO_withHubbard.py
 echo "Step 2: Ground, DOS, and Band without DFT+U."
-tsp mpirun -np $CORENUMBER dftsolve.py -i ZnO_woHubbard.py
+tsp dftsolve -p $CORENUMBER -i ZnO_woHubbard.py
 
 # Rocksalt TiC with Elastic Calculations
 echo "Adding: Rocksalt TiC."
 cd ../TiC-elastic-electronic
-tsp mpirun -np $CORENUMBER dftsolve.py -i TiC.py -g TiC_mp-631_primitive-Final.cif
+tsp dftsolve -p $CORENUMBER -i TiC.py -g TiC_mp-631_primitive-Final.cif
 
 # Phonon dispersion of Aluminum
 echo "Adding: Phonon dispersion of bulk Aluminum."
 cd ../Al-phonon
-tsp mpirun -np $CORENUMBER dftsolve.py -i Al-phonon.py -g Al_mp-134_primitive.cif
+tsp dftsolve -p $CORENUMBER -i Al-phonon.py -g Al_mp-134_primitive.cif
 
 # Finish
 echo "All calculations except the HSE calculation are added. Due to consuming too much time, please run the HSE example separately."
