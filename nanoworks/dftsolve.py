88dfd88011d5c927ccfb91bed6cda18a3597daa7
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2026-08-06 12:38:52 +0300

    fix: apply LCAO grid spacing correctly

--- nanoworks/dftsolve.py
+++ nanoworks/dftsolve.py
@@ -107,6 +107,7 @@ import requests
 import pickle
 import nanoworks
 from nanoworks.engine.gpaw import (
+    build_grid_spec,
     build_hybrid_xc,
     create_gpaw_calc,
     is_hybrid,
@@ -818,36 +819,28 @@ class dftsolve:
                 # Fix the spacegroup in the geometric optimization if wanted
                 if self.Fix_symmetry == True:
                     self.bulk_configuration.set_constraint(FixSymmetry(self.bulk_configuration))
-                if self.Ground_gpts_density is not None:
-                    calc_kwargs['kpts'] = build_kpoint_spec(
-                        density=self.Ground_kpts_density,
+                calc_kwargs['kpts'] = build_kpoint_spec(
+                    density=self.Ground_kpts_density,
+                    size=(
+                        self.Ground_kpts_x,
+                        self.Ground_kpts_y,
+                        self.Ground_kpts_z,
+                    ),
+                    gamma=self.Gamma,
+                )
+
+                calc_kwargs.update(
+                    build_grid_spec(
+                        spacing=self.Ground_gpts_density,
                         size=(
-                            self.Ground_kpts_x,
-                            self.Ground_kpts_y,
-                            self.Ground_kpts_z,
+                            self.Ground_gpts_x,
+                            self.Ground_gpts_y,
+                            self.Ground_gpts_z,
                         ),
-                        gamma=self.Gamma,
                     )
+                )
 
-                    calc = create_gpaw_calc(**calc_kwargs)
-                else:
-                    calc_kwargs['kpts'] = build_kpoint_spec(
-                        density=self.Ground_kpts_density,
-                        size=(
-                            self.Ground_kpts_x,
-                            self.Ground_kpts_y,
-                            self.Ground_kpts_z,
-                        ),
-                        gamma=self.Gamma,
-                    )
-
-                    calc_kwargs['gpts'] = (
-                        self.Ground_gpts_x,
-                        self.Ground_gpts_y,
-                        self.Ground_gpts_z,
-                    )
-
-                    calc = create_gpaw_calc(**calc_kwargs)
+                calc = create_gpaw_calc(**calc_kwargs)
 
                 # Wrapping for vdW
                 if hasattr(self.config, 'vdW_calc') and self.config.vdW_calc.upper() == 'D3':
